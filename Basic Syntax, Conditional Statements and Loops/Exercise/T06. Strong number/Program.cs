55d379684eb65085384a0c5331f3dc3fd9c8b617
Spas Ermenov <101284035+SpasErmenov@users.noreply.github.com>
2022-09-01 20:24:51 +0300

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/T06. Strong number/Program.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace T06_Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            long factorialSum = 0;
+            for (int i = 0; i <= number.Length - 1; i++)
+            {
+                char currCh = number[i];
+                int currDigit = (int)currCh - 48;
+
+                long currDigFactorial = 1;
+                for (int r = currDigit; r > 1; r--)
+                {
+                    currDigFactorial *= r;
+                }
+
+                factorialSum += currDigFactorial; 
+            }
+
+            if (factorialSum == int.Parse(number))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            
+        }
+    }
+}
