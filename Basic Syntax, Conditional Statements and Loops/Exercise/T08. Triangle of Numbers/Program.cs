3c207736dc2dbc18c65384e539d3da750f12f8a5
Spas Ermenov <101284035+SpasErmenov@users.noreply.github.com>
2022-09-01 20:26:39 +0300

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/T08. Triangle of Numbers/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace T08_Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.Write("\n");
+
+            }
+        }
+    }
+}
