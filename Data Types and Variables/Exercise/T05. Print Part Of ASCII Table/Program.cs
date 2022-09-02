49dfa201d41fb23796daefc570f8b13a84de77ae
Spas Ermenov <101284035+SpasErmenov@users.noreply.github.com>
2022-09-02 22:53:56 +0300

    Create Program.cs

---
+++ Data Types and Variables/Exercise/T05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace T05_Print_Part_Of_ASCII_Table2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                char currCh = (char)i;
+                Console.Write($"{currCh} ");
+
+            }
+        }
+    }
+}
